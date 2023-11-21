6dee7db6ad7e8d3a12fb98046631d9901813e08d
Gonzalo Delgado <gonzadelgado.geo@gmail.com>
2023-11-21 20:35:20 -0300

    creacion del proyecto

---
+++ servicio-salud/src/test/java/com/app/serviciosalud/ServicioSaludApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.serviciosalud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicioSaludApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
