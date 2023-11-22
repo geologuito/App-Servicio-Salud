63eecc5061b537d896ab48f95348c3ca919f9717
Ayelen05 <ayelenmarti@gmail.com>
2023-11-22 20:33:29 -0300

    Entidades completas
    Entidades con sus atributos y Data de lombok.

--- servicioSalud/src/main/java/com/app/servicioSalud/entidades/Profesional.java
+++ servicioSalud/src/main/java/com/app/servicioSalud/entidades/Profesional.java
@@ -1,6 +1,44 @@
 
 package com.app.servicioSalud.entidades;
 
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+
+@Entity
+@Data
 public class Profesional {
+    
+     @Id
+    private String Matricula;
+    
+     private String nombre;
+     private String apellido;
+     private String correo;
+     private String password;
+     private Boolean activo;
+     private String especialidad;
+     private Integer consulta;
+     private Date horario;
+  
+     @OneToMany
+     private Usuario usuario;
+     @OneToMany
+     private Turno turno;
+     @OneToMany
+     private HistoriaClinica historiaClinica;
+     
+     
+    
+     
+     
+     
+     
+     
     
 }
