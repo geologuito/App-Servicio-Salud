7e25356e37c3698439402d25c2f1db17a01445fd
Gonzalo Delgado <gonzadelgado.geo@gmail.com>
2023-11-24 00:29:43 -0300

    actualizo mi main local

---
+++ servicioSalud/src/main/java/com/app/servicioSalud/SeguridadWeb.java
@@ -0,0 +1,41 @@
+package com.app.servicioSalud;
+
+import com.app.servicioSalud.servicios.UsuarioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SeguridadWeb extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public UsuarioServicio usuarioServicio;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .antMatchers("/admin/").hasRole("ADMIN") // le da permiso solo a los admin para el paneladministrador
+                .antMatchers("/css/" , "/js/" ,"/img/*", "/**" )
+                .permitAll()
+                .and().formLogin()
+                        .loginPage("/login")
+                        .loginProcessingUrl("/logincheck")
+                        .usernameParameter("email")
+                        .passwordParameter("password")
+                        .defaultSuccessUrl("/inicio")
+                        .permitAll()
+                .and().logout()
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login")
+                        .permitAll()
+                .and().csrf()
+                        .disable();
+    }
+}
