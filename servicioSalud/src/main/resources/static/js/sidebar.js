b82b982a5846f2e0ef684e5ea35a96c26badfdca
Nahuel-Rey <nahuel_a_rey@hotmail.com>
2023-12-05 18:48:13 -0300

    correccion conflictos css

--- servicioSalud/src/main/resources/static/js/sidebar.js
+++ servicioSalud/src/main/resources/static/js/sidebar.js
@@ -1,7 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     var sidebar = document.getElementById('sidebar');
     var toggleBtn = document.getElementById('sidebar-toggle');
-<<<<<<< HEAD
 
     toggleBtn.addEventListener('click', function () {
         if (sidebar.style.width === '250px') {
@@ -13,17 +12,3 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
-=======
-  
-    toggleBtn.addEventListener('click', function () {
-      if (sidebar.style.width === '250px') {
-        sidebar.style.width = '0';
-        toggleBtn.innerHTML = '☰ Mostrar Perfil';
-      } else {
-        sidebar.style.width = '250px';
-        toggleBtn.innerHTML = '☰ Ocultar Perfil';
-      }
-    });
-  });
-  
->>>>>>> developer
