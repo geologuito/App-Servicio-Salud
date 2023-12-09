cbe1945b30fb3575a5f8c7ba796a6dd49e1a056c
Olga Betancourt <blom31@gmail.com>
2023-12-09 00:15:48 -0300

    Estilo de la página de crear historia desde el panel del Prof

--- servicioSalud/src/main/java/com/app/servicioSalud/controladores/ImagenControlador.java
+++ servicioSalud/src/main/java/com/app/servicioSalud/controladores/ImagenControlador.java
@@ -1,8 +1,8 @@
+
 package com.app.servicioSalud.controladores;
 
-import com.app.servicioSalud.entidades.Paciente;
+
 import com.app.servicioSalud.entidades.Profesional;
-import com.app.servicioSalud.servicios.PacienteServicio;
 import com.app.servicioSalud.servicios.ProfesionalServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -13,42 +13,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/imagen")
 public class ImagenControlador {
-
+    
     @Autowired
     ProfesionalServicio profesionalServicio;
-
-    @Autowired
-    PacienteServicio pacienteServicio;
-
-    @GetMapping("/profesional/{id}")
+    
+    @GetMapping("/perfil/{id}")
     public ResponseEntity<byte[]> imagenProfesional(@PathVariable String id) {
-
+    
         Profesional profesional = profesionalServicio.getOne(id);
-
+        
         byte[] imagen = profesional.getImagen().getContenido();
 
         HttpHeaders headers = new HttpHeaders();
-
+        
         headers.setContentType(MediaType.IMAGE_JPEG);
-
-        return new ResponseEntity<>(imagen, headers, HttpStatus.OK);
+        
+        return new ResponseEntity<>(imagen, headers , HttpStatus.OK);
     }
-
-    @GetMapping("/paciente/{id}")
-    public ResponseEntity<byte[]> imagenPaciente(@PathVariable String id) {
-
-        Paciente paciente = pacienteServicio.getOne(id);
-
-        byte[] imagen = paciente.getImagen().getContenido();
-
-        HttpHeaders headers = new HttpHeaders();
-
-        headers.setContentType(MediaType.IMAGE_JPEG);
-
-        return new ResponseEntity<>(imagen, headers, HttpStatus.OK);
-    }
+    
 }
