b6afe734296d7305ab5186831e8185e965fae906
Nahuel-Rey <nahuel_a_rey@hotmail.com>
2023-12-11 19:03:05 -0300

    11/12

--- servicioSalud/src/main/java/com/app/servicioSalud/controladores/PortalControlador.java
+++ servicioSalud/src/main/java/com/app/servicioSalud/controladores/PortalControlador.java
@@ -1,14 +1,23 @@
 package com.app.servicioSalud.controladores;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import com.app.servicioSalud.entidades.Profesional;
+import com.app.servicioSalud.servicios.ProfesionalServicio;
 
 
 @Controller
 @RequestMapping("/") // localhost:8080
 public class PortalControlador {
+
+    @Autowired
+    private ProfesionalServicio profesionalServicio;
 
     @GetMapping("/") // localhost:8080/ este queda en portal
     public String index() {
@@ -24,6 +33,15 @@ public class PortalControlador {
     public String profesionales() {
 
         return "profesionales";
+    }
+
+    @GetMapping("/especialidades")
+    public String especialidades(ModelMap modelo) {
+
+        List<Profesional> profesionales = profesionalServicio.listarProfesional();
+            modelo.addAttribute("profesionales", profesionales);
+
+        return "especialidades";
     }
     
 }
