b4d18283e585e3d26781c251bea1c9ef03b8ea43
Gonzalo Delgado <gonzadelgado.geo@gmail.com>
2023-12-14 11:59:40 -0300

    cambios

--- servicioSalud/src/main/java/com/app/servicioSalud/controladores/AdminControlador.java
+++ servicioSalud/src/main/java/com/app/servicioSalud/controladores/AdminControlador.java
@@ -1,16 +1,21 @@
 package com.app.servicioSalud.controladores;
 
+import com.app.servicioSalud.entidades.HistoriaClinica;
 import com.app.servicioSalud.entidades.Paciente;
 import com.app.servicioSalud.entidades.Profesional;
+import com.app.servicioSalud.entidades.Turno;
 import com.app.servicioSalud.excepciones.MiException;
+import com.app.servicioSalud.servicios.HistoriaClinicaServicio;
 import com.app.servicioSalud.servicios.PacienteServicio;
 import com.app.servicioSalud.servicios.ProfesionalServicio;
+import com.app.servicioSalud.servicios.TurnoServicio;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,16 +27,26 @@ public class AdminControlador {
     private PacienteServicio pacienteServicio;
     @Autowired
     private ProfesionalServicio profesionalServicio;
+    @Autowired
+    private HistoriaClinicaServicio HistoriaClinicaServicio;
+    @Autowired
+    private TurnoServicio TurnoServicio;
+
 
     @GetMapping("/dashboard")
     public String panelAdministrativo(ModelMap modelo) {
 
         List<Paciente> pacientes = pacienteServicio.listarPaciente();
         List<Profesional> profesionales = profesionalServicio.listarProfesional();
-        modelo.addAttribute("pacientes", pacientes);
-        modelo.addAttribute("profesional", profesionales);
+        List<HistoriaClinica> hc = HistoriaClinicaServicio.listarHC();
+        
 
-        return "dashboard";
+        modelo.addAttribute("pacientes", pacientes);
+        modelo.addAttribute("profesionales", profesionales);
+        modelo.addAttribute("hc",hc);
+        
+        
+        return "dashbo";
     }
 
     @GetMapping("/pacientes")
@@ -39,7 +54,7 @@ public class AdminControlador {
         List<Paciente> pacientes = pacienteServicio.listarPaciente();
         modelo.addAttribute("pacientes", pacientes);
 
-        return "pacienteList";
+        return "dashbo";
     }
 
     @GetMapping("/modificarPaciente/{dni}")
@@ -48,7 +63,22 @@ public class AdminControlador {
 
         pacienteServicio.modificarPaciente(archivo, dni, email, domicilio, telefono, password, password2);
 
-        return "redirect:/admin/pacientes";
+        return "redirect:/paciente/modificarPaciente.html";
+    }
+    @PostMapping("/modificar/{dni}")
+    public String modificar(@PathVariable String dni, String nombre, String apellido, String email, String domicilio, String telefono, String password,
+            String edad, MultipartFile archivo,
+            ModelMap modelo) {
+        try {
+            pacienteServicio.modificarPaciente(archivo, dni, email, domicilio, telefono, password, password);
+            return "dashbo"; // si esta todo ok va a ir a Dasboard- Esta regregando al login
+
+        } catch (MiException ex) {
+
+            modelo.put("error", ex.getMessage());
+            return "modificarPaciente.html"; // mapear con html
+
+        }
     }
 
     @GetMapping("/profesional")
